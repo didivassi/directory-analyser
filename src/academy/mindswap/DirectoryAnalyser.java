9c878d37be00a4e96b34d2e369b60a90f9a4ec6c
didivassi <diogovelho@gmail.com>
2021-05-31 16:37:51 +0100

    updated with Soraia's way

--- src/academy/mindswap/DirectoryAnalyser.java
+++ src/academy/mindswap/DirectoryAnalyser.java
@@ -63,7 +63,13 @@ public class DirectoryAnalyser {
         }
         File directory=askForDirectory();
         String fileToFilter = getUserInput("What's the filename to filter");
-        File[] names=directory.listFiles(new FilterFilesByStringName(fileToFilter));
+       // File[] names=directory.listFiles(new FilterFilesByStringName(fileToFilter));
+        File[] names=directory.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                return pathname.getName().startsWith(fileToFilter);
+            }
+        });
 
         assert names!=null;
         if(names.length==0){
